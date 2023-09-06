4818c7301cb445ddb86cacd600a546a9bf8db846
jaiminpanchal <jaiminpanchal229@gmail.com>
2023-09-06 15:05:30 +0530

    project

--- Website_Grid/js/Script.js
+++ Website_Grid/js/Script.js
@@ -8,3 +8,26 @@
             document.getElementById("mySidebar").style.width = "0";
             document.getElementById("main").style.marginLeft= "0";
           }
+
+
+
+          // 
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
