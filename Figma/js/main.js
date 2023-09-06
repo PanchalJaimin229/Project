04d638309b252c0441d0314dba5781af46d3abd4
jaiminpanchal <jaiminpanchal229@gmail.com>
2023-09-06 11:13:15 +0530

    .

--- Figma/js/main.js
+++ Figma/js/main.js
@@ -11,7 +11,29 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navmenu.classList.toggle('open');
 }
-// side bar
+// scroll bar //
+
+
+    window.addEventListener('scroll', reveal);
+
+    function reveal(){
+      var reveals = document.querySelectorAll('.reveal');
+
+      for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+          reveals[i].classList.add('active');
+        }
+        else{
+          reveals[i].classList.remove('active');
+        }
+      }
+    }
+
 
 
 
